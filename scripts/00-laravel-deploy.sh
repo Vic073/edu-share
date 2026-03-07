3937775f10077dc9e13a1ae91524dfd34c97c535
Victor Chilomo <chilomovictor@gmail.com>
2026-03-07 15:13:19 +0200

    added docker file

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "=== Running Laravel deployment tasks ==="
+
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
